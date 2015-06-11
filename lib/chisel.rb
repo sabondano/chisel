ba5f379bca5aea6127e6851d46b29e377f9e196e
Sebastian Abondano <sabondano1@gmail.com>
2015-06-10 20:21:05 -0600

    Finalized chisel.rb for it to work according to the spec (base expectations).

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,13 +1,22 @@
 require_relative '../lib/markdown_parser'
+require_relative '../lib/emphasis_renderer'
+require_relative '../lib/header_renderer'
+require_relative '../lib/link_renderer'
+require_relative '../lib/list_renderer'
+require_relative '../lib/paragraph_renderer'
+require_relative '../lib/special_character_renderer'
+require_relative '../lib/strong_renderer'
 
 input_filename = ARGV[0]
 output_filename = ARGV[1]
+input = File.read(input_filename)
 
-result = MarkdownParser.new(input_filename).chisel
+result = MarkdownParser.new(input).chisel
 File.write(output_filename, "#{result}")
+output = File.read(output_filename)
 
-lines_input = 6
-lines_output = 8
+lines_input  = input.lines.count
+lines_output = output.lines.count
 
 puts "Converted #{input_filename} (#{lines_input} lines) to #{output_filename} (#{lines_output} lines)"
 
---
+++ my_input.markdown
@@ -0,0 +1,18 @@
+# My Life in Desserts
+
+## Chapter 1: The Beginning
+
+"You just *have* to try the cheesecake," he said. "Ever since it appeared in
+**Food & Wine** this place has been packed every night."
+
+My favorite cuisines are:
+
+* Sushi
+* Barbeque
+* Mexican
+
+1. Sushi
+2. Barbeque
+3. Mexican
+
+[This link](http://example.net/) has no title attribute.
